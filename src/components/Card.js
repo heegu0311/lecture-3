86bbe79cebb2ef6af66423baf5d000cf9b1a00e5
joseph <heegu0311@gmail.com>
2025-01-26 14:42:52 +0900

    fix: image lazy load (intersection observer)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 function Card(props) {
+	const imgRef = useRef(null)
+
+	useEffect(() => {
+		const options = {}
+		const cb = (entries, observer) => {
+			console.log(entries)
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					console.log('is intersecting', entry.isIntersecting)
+					entry.target.src = entry.target.dataset.src
+					observer.unobserve(entry.target)
+				}
+			})
+		}
+
+		const observer = new IntersectionObserver(cb, options)
+
+		observer.observe(imgRef.current)
+
+		return () => {
+			observer.disconnect()
+		}
+	}, []);
+
+
 	return (
 		<div className="Card text-center">
-			<img src={props.image}/>
+			<img data-src={props.image} ref={imgRef}/>
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
